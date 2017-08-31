f09fe681424247a8535031b3a16f3c59a2390495
admin@kolabrate.com.au <Ana1982(>
2017-08-31 11:14:23 +1000

    Included IMovie

---
+++ Showtime.Service/IMovie.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Movie.Models;
+
+namespace Movie.Service
+{
+    public interface IMovie
+    {
+       List<MovieInfo> GetShowTimesForaMovie(string movieName);
+    }
+}
