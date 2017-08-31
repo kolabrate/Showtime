cbbcbcc6a2c2dec82a7a5ebbf6f725c0dfdfe1f5
admin@kolabrate.com.au <Ana1982(>
2017-08-31 11:48:10 +1000

    Included Location & Showtime class.

--- Showtime.Models/MovieInfo.cs
+++ Showtime.Models/MovieInfo.cs
@@ -6,13 +6,20 @@ namespace Movie.Models
     public class MovieInfo
     {
         public string Name { get; set; }
+        public int Duration { get; set; }
         public List<ShowTime> ShowTimes { get; set; }
 
     }
-
     public class ShowTime
     {
         public DateTime Start { get; set; }
+        public Location Location { get; set; }
+    }
+
+    //To be done - Location class needs more properties.
+    public class Location 
+    {
+        public string Theater { get; set; }
 
     }
 
